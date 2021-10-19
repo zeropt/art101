ba47a0868acfb95bb6631a1a33453300ee54050e
zeropt <ferr0fluidmann@gmail.com>
2021-10-18 19:50:24 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Authors: Raymond Wong, Riley Mann
+ * Created: 18 OCT 2021
+ * License: Public Domain
+ */
+
+// Define Variables
+var make = "Subaru";
+var model = "Forester";
+var color = "Navy Blue";
+var year = 2019;
+
+// calculate
+var age = 2022 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
